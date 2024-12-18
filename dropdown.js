fd4efba11f0df5867395b315f1ad4a4b63537ee4
SoftmapYT <softmapyt@gmail.com>
2024-12-18 15:44:16 +0000

    done chages\\

--- dropdown.js
+++ dropdown.js
@@ -1,12 +1,12 @@
 const dropdownToggle = document.querySelectorAll('.dropdown-toggle');
-			const dropdownMenu = document.querySelectorAll('.dropdown-menu');
+const dropdownMenu = document.querySelectorAll('.dropdown-menu');
 
-			dropdownToggle.forEach((toggle, index) => {
-  				toggle.addEventListener('mouseover', () => {
-    				dropdownMenu[index].style.display = 'block';
-  				});
+dropdownToggle.forEach((toggle, index) => {
+  	toggle.addEventListener('mouseover', () => {
+		dropdownMenu[index].style.display = 'block';
+	});
 
-  				toggle.addEventListener('mouseout', () => {
-  					dropdownMenu[index].style.display = 'none';
-  				});
-			});
+    toggle.addEventListener('mouseout', () => {
+		dropdownMenu[index].style.display = 'none';
+	});
+});
